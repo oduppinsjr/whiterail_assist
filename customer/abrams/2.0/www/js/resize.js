2a9c7b2c64bef86026964bbaf67979e2da753c58
Odell Duppins Jr <35476033+oduppinsjr@users.noreply.github.com>
2021-04-25 21:48:10 -0400

    Delete customer/abrams directory

--- customer/abrams/2.0/www/js/resize.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener('message', function(message) {
-    if (message.data.resizeMe) {
-        document.getElementsByClassName('text-frame')[0].height =
-            message.data.resizeMe + "px";
-    }
-});
